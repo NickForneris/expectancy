1e3910749fc17c440fdba1ac6ab04096665e3a5d
Nick Forneris <nicholasdforneris@gmail.com>
2022-12-06 01:58:07 -0600

    Updates

---
+++ precache-manifest.6dc2e94a3a797816823f8b8f5ddef950.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "370a12f73d58d8285557",
+    "url": "/expectancy/static/css/main.216a562a.chunk.css"
+  },
+  {
+    "revision": "370a12f73d58d8285557",
+    "url": "/expectancy/static/js/main.370a12f7.chunk.js"
+  },
+  {
+    "revision": "518d4b8de7eccf59a226",
+    "url": "/expectancy/static/css/1.46678f25.chunk.css"
+  },
+  {
+    "revision": "518d4b8de7eccf59a226",
+    "url": "/expectancy/static/js/1.518d4b8d.chunk.js"
+  },
+  {
+    "revision": "3f6739947822ee9878f8",
+    "url": "/expectancy/static/js/runtime~main.3f673994.js"
+  },
+  {
+    "revision": "4fcab6d2c4b93f78dc3176fb244d38ad",
+    "url": "/expectancy/index.html"
+  }
+];
